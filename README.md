40db3f640a262fdc76cf650d258d1f2546442688
Thomas Kottke <t.kottke90@gmail.com>
2023-11-12 20:08:46 -0600

    refactor: mv js files to js folder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LangChain Playground
+
+I am interested in learning about the capabilities of LangChain as an extension of Large Language Models (LLMs).  Specifically in the capability to create customized side effects from a conversation as well as configure embeddings for long term memory.
--- javascript/scripts/hugging-face-local.js
+++ javascript/scripts/hugging-face-local.js
@@ -1,11 +1,12 @@
 import { HuggingFaceInference } from "langchain/llms/hf";
+import {} from 'langchain/llms/'
 import { DynamicTool } from 'langchain/tools';
 import { config } from 'dotenv';
 
 config();
 
 const model = new HuggingFaceInference({
-  model: "gpt2",
+  model: "openchat/openchat_3.5",
   apiKey: process.env.HUGGINGFACEHUB_API_KEY, // In Node.js defaults to process.env.HUGGINGFACEHUB_API_KEY
   temperature: 0.9
 });
